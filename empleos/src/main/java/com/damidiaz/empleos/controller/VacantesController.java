e4c2a5cc324636e3387170b5ec0b1b544ba9afc4
Damian Diaz <Damian Diaz@DESKTOP-9FCB10N>
2021-02-23 19:15:10 -0300

    prueba commit

--- empleos/src/main/java/com/damidiaz/empleos/controller/VacantesController.java
+++ empleos/src/main/java/com/damidiaz/empleos/controller/VacantesController.java
@@ -1,10 +1,22 @@
 package com.damidiaz.empleos.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
 
+	@GetMapping("/view/{id}")
+	 public String verDetalle(@PathVariable("id")int idVacante,Model model) {
+		
+		System.out.println("id vacante:" + idVacante);
+		model.addAttribute("idVacante",idVacante);
+		
+		return "vacantes/detalle";
+		
+	}
 }
