48ff627957a4ecb496aeeae83488dc546b49f1fc
damianDev22 <41934075+damianDev22@users.noreply.github.com>
2021-02-27 01:48:55 -0300

    27/02/2021

---
+++ empleos/src/main/java/com/damidiaz/empleos/service/CategoriasServiceImpl.java
@@ -0,0 +1,27 @@
+package com.damidiaz.empleos.service;
+
+import java.util.List;
+
+import com.damidiaz.empleos.model.Categoria;
+
+public class CategoriasServiceImpl implements CategoriasService {
+
+	@Override
+	public void guardar(Categoria categoria) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Categoria> buscarTodas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Categoria buscarPorId(Integer idCategoria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
