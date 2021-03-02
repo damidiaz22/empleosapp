8b6cb9689a836fac6fa2551ff47e35c5e102d0f3
damianDev22 <41934075+damianDev22@users.noreply.github.com>
2021-03-01 23:20:19 -0300

    1/3/2021

--- empleos/src/main/java/com/damidiaz/empleos/controller/CategoriasController.java
+++ empleos/src/main/java/com/damidiaz/empleos/controller/CategoriasController.java
@@ -1,33 +1,90 @@
 package com.damidiaz.empleos.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.damidiaz.empleos.model.Categoria;
+import com.damidiaz.empleos.service.CategoriasService;
 
 @Controller
 @RequestMapping(value="/categorias")
 public class CategoriasController {
 
-	// @GetMapping("/index")
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	
+	
+	@Autowired
+	private CategoriasService serviceCategorias;
+	
+	
+	
+	
+	@GetMapping("/index")
+	//@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String mostrarIndex(Model model) {
+		
+		
+		List<Categoria> listaCategorias = this.serviceCategorias.buscarTodas();
+		model.addAttribute("categorias",listaCategorias);
+		
+		
 		return "categorias/listCategorias";
 	}
 
-	// @GetMapping("/create")
-	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	 @GetMapping("/create")
+	//@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public String crear() {
+		 
 		return "categorias/formCategoria";
 	}
 
-	 //@PostMapping("/save")
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String guardar(@RequestParam("nombre") String nombre,@RequestParam("descripcion") String descripcion) {
-		System.out.println("Categoria:" + nombre );
-		System.out.println("descripcion:" + descripcion );
-		return "categorias/listCategorias";
+//	 //@PostMapping("/save")
+//	@RequestMapping(value = "/save", method = RequestMethod.POST)
+//	public String guardar(@RequestParam("nombre") String nombre,@RequestParam("descripcion") String descripcion) {
+//		System.out.println("Categoria:" + nombre );
+//		System.out.println("descripcion:" + descripcion );
+//		return "categorias/listCategorias";
+//	}
+	 
+	 
+	@PostMapping("/save")
+	public String guardar(Categoria categoria,BindingResult result,RedirectAttributes attributes) {
+
+		if(result.hasErrors()) {
+			
+			for(ObjectError error : result.getAllErrors()) {
+				System.out.println("Ocurrio un error:" + error.getDefaultMessage());
+			}
+			
+			return "categoria/formCategorias";
+		}
+		
+		this.serviceCategorias.guardar(categoria);
+		attributes.addFlashAttribute("msj","Registro guardado");
+		
+		return "redirect:/categorias/index";
 	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		webDataBinder.registerCustomEditor(Date.class,new CustomDateEditor(dateFormat,false));
+		
+		
+	}
+	
 }
--- empleos/src/main/java/com/damidiaz/empleos/service/CategoriasServiceImpl.java
+++ empleos/src/main/java/com/damidiaz/empleos/service/CategoriasServiceImpl.java
@@ -1,21 +1,65 @@
 package com.damidiaz.empleos.service;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
 
 import com.damidiaz.empleos.model.Categoria;
 
+
+@Service
 public class CategoriasServiceImpl implements CategoriasService {
+
+	
+	private List<Categoria> lista = null;
+	
+	public CategoriasServiceImpl() {
+		
+		lista =  new LinkedList<Categoria>();
+		
+		try {
+			
+			Categoria categoria1 = new Categoria();
+			categoria1.setId(1);
+			categoria1.setNombre("Ventas");
+			categoria1.setDescripcion("");
+			lista.add(categoria1);
+			
+			Categoria categoria2 = new Categoria();
+			categoria2.setId(2);
+			categoria2.setNombre("Sistemas");
+			categoria2.setDescripcion("");
+			lista.add(categoria2);
+			
+			Categoria categoria3 = new Categoria();
+			categoria3.setId(3);
+			categoria3.setNombre("Finanzas");
+			categoria3.setDescripcion("");
+			lista.add(categoria3);
+			
+			Categoria categoria4 = new Categoria();
+			categoria4.setId(4);
+			categoria4.setNombre("Bancario");
+			categoria4.setDescripcion("");
+			lista.add(categoria4);
+			
+			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
 
 	@Override
 	public void guardar(Categoria categoria) {
-		// TODO Auto-generated method stub
+		this.lista.add(categoria);
 		
 	}
 
 	@Override
 	public List<Categoria> buscarTodas() {
-		// TODO Auto-generated method stub
-		return null;
+		return lista;
 	}
 
 	@Override
