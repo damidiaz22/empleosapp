f5b5a451f8c29216186b351136d736de33169c8c
damianDev22 <41934075+damianDev22@users.noreply.github.com>
2021-05-11 22:42:24 -0300

    11/5/2021

--- empleos/src/main/java/com/damidiaz/empleos/EmpleosApplication.java
+++ empleos/src/main/java/com/damidiaz/empleos/EmpleosApplication.java
@@ -2,7 +2,7 @@ package com.damidiaz.empleos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//hola
+//holacomova
 @SpringBootApplication
 public class EmpleosApplication {
 
